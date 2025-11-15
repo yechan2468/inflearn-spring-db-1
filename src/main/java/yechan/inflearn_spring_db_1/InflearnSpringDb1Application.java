ce4dcbbfb0799a70bed3638f530b2cef29e82d9d
Yechan Lee <yechan24680@gmail.com>
2025-11-15 19:46:40 +0900

    first commit

---
+++ src/main/java/yechan/inflearn_spring_db_1/InflearnSpringDb1Application.java
@@ -0,0 +1,13 @@
+package yechan.inflearn_spring_db_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringDb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringDb1Application.class, args);
+	}
+
+}
---
+++ src/test/java/yechan/inflearn_spring_db_1/InflearnSpringDb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package yechan.inflearn_spring_db_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringDb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
